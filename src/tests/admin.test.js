a99c13d8508f032a99320b25be6ac4b0fcd5c4f7
melitus <asmelitus@gmail.com>
2022-04-18 12:53:27 +0100

    feat: add an integration testing for the admin operations

---
+++ src/tests/admin.test.js
@@ -0,0 +1,65 @@
+const request = require('supertest');
+const app = require('../loaders');
+
+describe('Admin', () => {
+  describe('/admin/best-profession', () => {
+    it('should return profession with the highest income within given time range', async () => {
+      const { statusCode, body } = await request(app)
+        .get('/v1/api/admin/best-profession')
+        .query({ startDate: '2020-08-15T19:11:26.737Z' })
+        .query({ endDate: '2020-08-17T19:11:26.737Z' });
+      expect(statusCode).toEqual(200);
+      expect(body.data).toEqual(
+        expect.objectContaining({
+          profession: 'Programmer',
+          earned: 542,
+        }),
+      );
+    });
+
+    it('should return null if there are no jobs within given time range', async () => {
+      const { statusCode, body } = await request(app)
+        .get('/v1/api/admin/best-profession')
+        .query({ startDate: '2021-08-10T09:00:00.000Z' })
+        .query({ endDate: '2021-08-16T23:59:59.000Z' });
+
+      expect(statusCode).toEqual(404);
+      expect(body.message).toEqual('No best profession found');
+    });
+  });
+
+  describe('/admin/best-clients', () => {
+    it('should return list of clients who paid most within given time range', async () => {
+      const actualValue = [{"fullName": "Harry Potter", "id": 1, "paid": 421}, {"fullName": "Mr Robot", "id": 2, "paid": 321}]
+      const { statusCode, body } = await request(app)
+        .get('/v1/api/admin/best-clients')
+        .query({ startDate: '2020-08-15T19:11:26.737Z' })
+        .query({ endDate: '2020-08-17T19:11:26.737Z' });
+
+      expect(statusCode).toEqual(200);
+      expect(body.data).toEqual((actualValue));
+    });
+
+    it('should limit the list by query param', async () => {
+      const { statusCode, body } = await request(app)
+        .get('/v1/api/admin/best-clients')
+        .query({ startDate: '2020-08-15T19:11:26.737Z' })
+        .query({ endDate: '2020-08-17T19:11:26.737Z' })
+        .query({ limit: 1 });
+
+      expect(statusCode).toEqual(200);
+      expect(body.data).toHaveLength(1);
+      expect(body.data[0]).toEqual(expect.objectContaining({ fullName: 'Harry Potter', id: 1, paid: 421 }));
+    });
+
+    it('should return [] if there are no jobs within given time range', async () => {
+      const { statusCode, body } = await request(app)
+        .get('/v1/api/admin/best-clients')
+        .query({ startDate: '2021-08-10T09:00:00.000Z' })
+        .query({ endDate: '2021-08-16T23:59:59.000Z' });
+
+      expect(statusCode).toEqual(200);
+      expect(body.data).toHaveLength(0);
+    });
+  });
+});
