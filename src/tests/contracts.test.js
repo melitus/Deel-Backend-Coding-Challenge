4115b21ee149a50e89bf5a283d701ed65936101d
melitus <asmelitus@gmail.com>
2022-04-18 12:53:56 +0100

    feat: add an integration testing for the contract operations

---
+++ src/tests/contracts.test.js
@@ -0,0 +1,58 @@
+const request = require('supertest');
+
+const app = require('../loaders');
+
+describe('Contracts', () => {
+  describe('/v1/api/contracts/:id', () => {
+
+    it('should return 404 when contract not found', async () => {
+      await request(app)
+        .get('/v1/api/contracts/199')
+        .set('profile_id', '5')
+        .expect(404);
+    });
+
+    it('should return contract when profile_id header matches with client', async () => {
+      const { statusCode, body } = await request(app)
+        .get('/v1/api/contracts/1')
+        .set('profile_id', '1');
+
+      expect(statusCode).toEqual(200);
+      expect(body.data).toMatchObject({
+        id: 1,
+        terms: 'bla bla bla',
+        status: 'terminated',
+        ClientId: 1,
+        ContractorId: 5,
+      });
+    });
+
+    it('should return contract when profile_id header matches with contractor', async () => {
+      const { statusCode, body } = await request(app)
+        .get('/v1/api/contracts/1')
+        .set('profile_id', '5');
+
+      expect(statusCode).toEqual(200);
+      expect(body.data).toMatchObject({
+        id: 1,
+        terms: 'bla bla bla',
+        status: 'terminated',
+        ClientId: 1,
+        ContractorId: 5,
+      });
+    });
+  });
+
+  describe('/contracts', () => {
+    it('should return not terminated contracts for the client', async () => {
+        const actualValue = [{"ClientId": 4, "ContractorId": 7, "createdAt": "2022-04-17T13:29:05.340Z", "id": 7, "status": "in_progress", "terms": "bla bla bla", "updatedAt": "2022-04-17T13:29:05.340Z"}, {"ClientId": 4, "ContractorId": 6, "createdAt": "2022-04-17T13:29:05.340Z", "id": 8, "status": "in_progress", "terms": "bla bla bla", "updatedAt": "2022-04-17T13:29:05.340Z"}, {"ClientId": 4, "ContractorId": 8, "createdAt": "2022-04-17T13:29:05.340Z", "id": 9, "status": "in_progress", "terms": "bla bla bla", "updatedAt": "2022-04-17T13:29:05.340Z"}]
+      const { statusCode, body } = await request(app)
+        .get('/v1/api/contracts')
+        .set('profile_id', '4');
+
+      expect(statusCode).toEqual(200);
+      expect(body.data).toHaveLength(3);
+      expect(body.data).toEqual(actualValue);
+    });
+  });
+});
